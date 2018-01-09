3b53b9e40f0986049248b597eb205ccf410c7666
George Dakhil <georgedakhil@gmail.com>
2018-01-09 09:59:49 -0400

    Se añadio cargar imagenes, Se añandio nuevos metodos para socket.io

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,10 @@ const views = express.Router();
 
 views.get('/', controllers.views.main);
 views.get('/star', controllers.views.estrella);
-views.get('/victoria', controllers.views.victoria);
+views.get('/victoria/:token', controllers.views.victoria);
 
 module.exports = {
     views
 }
+
+// eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyM2E5OTJlMDEyNDllNWEwIiwiaWF0IjoxNTE1NTAwNjU4fQ.-hGzmk9R0ebqg3lcZe73lL6XplOmBQtLtV1LIr33nII
