19e518bb72fbba63e5d078bda39971a780981b7b
George Dakhil <GeorgeDakhil@gmail.com>
2018-01-12 12:20:42 -0430

    Ruleta sin token

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,8 @@ const views = express.Router();
 views.get('/', controllers.views.main);
 views.get('/star', controllers.views.estrella);
 views.get('/victoria/:token', controllers.views.victoria);
+views.get('/victoria', controllers.views.victoria);
 
 module.exports = {
     views
 }
-
-// eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyM2E5OTJlMDEyNDllNWEwIiwiaWF0IjoxNTE1NTAwNjU4fQ.-hGzmk9R0ebqg3lcZe73lL6XplOmBQtLtV1LIr33nII
