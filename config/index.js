b812ff3e17ba5cbb30001ce168017c896c827d36
ebisuu_senior-at-048629946922 <carlos.velazquez@aituring.co>
2021-08-13 19:40:50 -0500

    prueba de sitio ssl 01

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     PORT: process.env.PORT || 6500,
-    DB: process.env.DB || 'mongodb://localhost:27017/ruleta?ssl=true',
+    DB: process.env.DB || 'mongodb://localhost:27017/ruleta',
     SECRET_TOKEN	: '$2a$12$hii2ldaOwCktin0LNxNAO.'
 };
